5346e199d3fb3d4bf74d4adc664b5a69b48174f4
endue <809653838@qq.com>
2021-11-15 23:29:31 +0800

    RateLimiterController源码分析

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
@@ -34,10 +34,13 @@ public class RateLimiterController implements TrafficShapingController {
     private final int maxQueueingTimeMs;
     /**
      * 每秒通过的请求数，也就是每个请求平均间隔恒定为 1000 / count ms
-     * 这里可以看出，该中方式无法满足QPS >1000的场景
+     * 这里可以看出，该种方式无法满足QPS >1000的场景
      */
     private final double count;
 
+    /**
+     * 最近获取到token的时间戳
+     */
     private final AtomicLong latestPassedTime = new AtomicLong(-1);
 
     public RateLimiterController(int timeOut, double count) {
@@ -52,7 +55,7 @@ public class RateLimiterController implements TrafficShapingController {
 
     /**
      * 通过阅读该方法的相关源码可以知道，当突然收到大量请求时，也是会有短暂的洪峰的。
-     * 在过一段时间后才会到达匀速模式，下面是测试用例{@link com.alibaba.csp.sentinel.demo.flow.PaceFlowDemo#simulatePulseFlow()}的日志：
+     * 在过一段时间后才会到达匀速模式，下面是测试用例{@link com.alibaba.csp.sentinel.demo.flow.PaceFlowDemo#simulatePulseFlow}的日志：
      * 1634981070077 one request pass, cost 100 ms
      * 1634981070077 one request pass, cost 100 ms
      * 1634981070077 one request pass, cost 100 ms
@@ -86,7 +89,7 @@ public class RateLimiterController implements TrafficShapingController {
         long costTime = Math.round(1.0 * (acquireCount) / count * 1000);
 
         // Expected pass time of this request.
-        // 在上次通过时间的基础上加上本次的耗时，得到期望通过的时间点
+        // 在上次成功获取token的时间基础上再加上本次的耗时，得到期望通过的时间点
         long expectedTime = costTime + latestPassedTime.get();
 
         // 如果期望时间 <= 当前时间，那么说明当前令牌充足可以放行，同时将当前时间设置为上次通过时间
@@ -103,17 +106,16 @@ public class RateLimiterController implements TrafficShapingController {
             if (waitTime > maxQueueingTimeMs) {
                 return false;
             } else {
-                // 需要等待时间 <= 设置的最大等待时长
-                // 由于此时可以等待，那么立即更新下一次通过的时间，当下一个请求到达时，会基于当前请求的通过时间来计算所需等待的时间
-                // 比如：请求1、请求2依次到达，latestPassedTime为10:01:05，
-                // 请求1需要等待1s，于是更新latestPassedTime为10:01:06，当请求2到达时，获取的latestPassedTime将会是请求1更新后的值
+                // 需要等待时间 <= 设置的最大等待时长，可以等待
+                // 首先更新最近获取token的时间，当下一个请求到达时，会基于最新的token被获取的时间来计算自己所需等待的时间
+                // 比如：请求1、请求2同时到达，latestPassedTime此时为10:01:05，
+                // 请求1CAS调用addAndGet()成功，更新latestPassedTime为10:01:06
+                // 请求2CAS首次失败，再次调用addAndGet()成功，更新latestPassedTime为10:01:07
                 long oldTime = latestPassedTime.addAndGet(costTime);
                 try {
-                    // 重新计算需要等待的实际，由于并发的原因此时可能请求2先到达更新了latestPassedTime
-                    // 所以需要重新计算一个请求1的等待时间
+                    // 重新计算所需等待时间，上一步即使并发调用addAndGet()最终也会让请求排队来获取自己所需的等待时间
                     waitTime = oldTime - TimeUtil.currentTimeMillis();
                     // 超过了阈值，那么由于上面更新了latestPassedTime，所以这里要减掉
-                    // todo 减掉执行前，其他请求进来，有可能会被拒绝掉
                     if (waitTime > maxQueueingTimeMs) {
                         latestPassedTime.addAndGet(-costTime);
                         return false;
